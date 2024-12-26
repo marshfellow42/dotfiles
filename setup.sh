8ae59322a25dcffb601cfc6edd78e663911a3330
Leandro Cândido <123888466+marshfellow42@users.noreply.github.com>
2024-12-25 23:15:18 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-pacman -Syu hyprcursor hyprlock hypridle hyprshot rofi-wayland jq swww waybar
+pacman -Syu git hyprcursor hyprlock hypridle hyprshot rofi-wayland jq swww waybar
+
+git clone https://aur.archlinux.org/yay.git || { echo "Error: Failed to clone yay"; exit 1; }
+cd yay || { echo "Error: Failed to access yay directory"; exit 1; }
+makepkg -si || { echo "Error: Failed to build yay"; exit 1; }
+cd ../
