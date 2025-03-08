10c3338d45e01a3e19a4c196b4e872b13e666e78
Leandro Cândido <123888466+marshfellow42@users.noreply.github.com>
2025-03-08 11:28:42 -0300

    Delete swww_afternoon.sh

--- swww_afternoon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if the flag for the afternoon exists
-if [ ! -f /tmp/swww_afternoon_flag ]; then
-    # Run the swww-img command
-    swww-img
-    # Create the flag file to indicate it has run
-    touch /tmp/swww_afternoon_flag
-fi
