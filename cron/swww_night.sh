1f90f8e855d742c329bd7dd729674e3c50deddbd
Leandro Cândido <123888466+marshfellow42@users.noreply.github.com>
2025-03-08 12:47:31 -0300

    Delete cron/swww_night.sh

--- cron/swww_night.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if the flag for the morning exists
-if [ ! -f /tmp/swww_night_flag ]; then
-    # Run the swww-img command
-    swww-img
-    # Create the flag file to indicate it has run
-    touch /tmp/swww_night_flag
-fi
