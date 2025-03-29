2b98ac5821c60a816d33757fece0f2fc81bd2cc6
Leandro Cândido <123888466+marshfellow42@users.noreply.github.com>
2025-03-29 00:42:13 -0300

    Update gaming.sh

--- install/apps/gaming.sh
+++ install/apps/gaming.sh
@@ -10,19 +10,12 @@ if ! command -v flatpak &> /dev/null; then
    exit 1
 fi
 
-# Install Pacman Packages
-echo "Installing Pacman Packages..."
-sudo pacman -S --noconfirm lutris dolphin-emu retroarch
+echo "Installing Arch Packages..."
+yay -S --noconfirm rpcs3-bin cemu ryujinx shadps4-git vita3k-git lutris dolphin-emu retroarch
 
-# Install Yay Packages
-echo "Installing Yay Packages..."
-yay -S --noconfirm rpcs3-bin cemu ryujinx shadps4-git vita3k-git
-
-# Install Flatpak Applications from Flathub
 echo "Installing Flatpak Apps from Flathub..."
 flatpak install -y flathub net.pcsx2.PCSX2 org.duckstation.DuckStation org.ppsspp.PPSSPP org.prismlauncher.PrismLauncher com.heroicgameslauncher.hgl
 
-# Install Flatpak Links
 echo "Installing Flatpak Links..."
 flatpak install --user -y https://sober.vinegarhq.org/sober.flatpakref
 
