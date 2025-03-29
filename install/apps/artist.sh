efec72c97036a934cfdbe375d0bec6f1508d7bd6
Leandro Cândido <123888466+marshfellow42@users.noreply.github.com>
2025-03-29 00:38:47 -0300

    Update artist.sh

--- install/apps/artist.sh
+++ install/apps/artist.sh
@@ -10,26 +10,22 @@ if ! command -v flatpak &> /dev/null; then
    exit 1
 fi
 
-# Install Pacman Packages
-echo "Installing Pacman Packages..."
-sudo pacman -S --noconfirm krita blender tenacity lmms
+echo "Installing Arch Packages..."
+yay -S --noconfirm beeref aseprite krita blender audacity lmms
 
-# Install Yay Packages
-echo "Installing Yay Packages..."
-yay -S --noconfirm beeref aseprite
-
-# Install Flatpak Applications from Flathub
 echo "Installing Flatpak Apps from Flathub..."
 flatpak install -y flathub com.obsproject.Studio org.kde.kdenlive
 
-# Install Flatpak Links
 echo "Installing Flatpak Links..."
 flatpak install -y https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
 
 echo "Installing GIMP plugin..."
-curl -O https://github.com/kamilburda/batcher/releases/download/1.0-RC3/batcher-1.0-RC3.zip
-unzip batcher-1.0-RC3.zip
-mv batcher ~/.config/GIMP/plug-ins/
-rm batcher-1.0-RC3.zip
+URL="https://github.com/kamilburda/batcher/releases/download/1.0/batcher-1.0.zip"
+FILENAME=$(basename "$URL")
+DIRNAME="${FILENAME%.zip}"
+curl -O "$URL"
+unzip "$FILENAME"
+mv "$DIRNAME/batcher" ~/.config/GIMP/plug-ins/
+rm -r "$FILENAME" "$DIRNAME"
 
 echo "Installation complete!"
